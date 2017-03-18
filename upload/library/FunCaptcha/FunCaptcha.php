cbc917e367c968c15077941e1b3bb1c44a59693a
Kevin Gosschalk <kevin@funcaptcha.com>
2017-03-18 13:57:38 +1000

    Convert away from buffering

--- upload/library/FunCaptcha/FunCaptcha.php
+++ upload/library/FunCaptcha/FunCaptcha.php
@@ -28,11 +28,11 @@ class FunCaptcha_FunCaptcha extends XenForo_Captcha_Abstract
     }
 
     public function isValid(array $input)
-	{
-		$options = XenForo_Application::get('options')->getOptions();
+    {
+        $options = XenForo_Application::get('options')->getOptions();
         if (empty($options['funcaptcha_public']) || empty($options['funcaptcha_private'])) {
             return true;
-        }		
+        }       
 
         require_once('funcaptcha-lib.php');
 
@@ -40,10 +40,10 @@ class FunCaptcha_FunCaptcha extends XenForo_Captcha_Abstract
         $funcaptcha->setProxy($options['funcaptcha_proxy']);
         $funcaptcha->setTheme($options['funcaptcha_theme']);
         $funcaptcha->setNoJSFallback($options['funcaptcha_javascript']);
-		$score =  $funcaptcha->checkResult($options['funcaptcha_private']);
-		
-		if ($score) {
-			return true;
+        $score =  $funcaptcha->checkResult($options['funcaptcha_private']);
+        
+        if ($score) {
+            return true;
         } else {
             $this->error = 'funcaptcha_unverfied';
             return false;
@@ -51,12 +51,12 @@ class FunCaptcha_FunCaptcha extends XenForo_Captcha_Abstract
 
     }
 
-	public function renderInternal(XenForo_View $view)
-	{
-		$options = XenForo_Application::get('options')->getOptions();
+    public function renderInternal(XenForo_View $view)
+    {
+        $options = XenForo_Application::get('options')->getOptions();
         if (empty($options['funcaptcha_public']) || empty($options['funcaptcha_private'])) {
             return '';
-        }		
+        }       
         
         require_once('funcaptcha-lib.php');
         
@@ -70,57 +70,49 @@ class FunCaptcha_FunCaptcha extends XenForo_Captcha_Abstract
         $output = $output . "<span class='fc-ctn'>";
         $output = $output . "<div id='funcaptcha' data-pkey='{$options['funcaptcha_public']}'></div>";
         $output = $output . "</span></li></div></div>";
-
-        ob_clean();
-        ?>
-        <script src="https://funcaptcha.com/fc/api/" async defer></script>
-        <script>
-        if (typeof fc_query_created == 'undefined') {
-            var fc_pkey = "<?= $options["funcaptcha_public"] ?>";
-            var fc_query_created = true;
-            jQuery(".footerLinks .OverlayTrigger").click(function() {
-                jQuery(".xenOverlay").children("form").each(function(){
-                    if ($(this).attr("action").indexOf("contact") >= 0) {
-                        $(this).find(".fc-ctn").html("<div id='funcaptcha' data-pkey='"+fc_pkey+"'></div>");
-                    } else if ($(this).attr("action").indexOf("lost") >= 0) {
-                        $(this).find(".fc-ctn").html("");
-                    }
-                });
-                try {
-                    FunCaptcha();
-                } catch (e) {
-                    
-                }
-            });
-            jQuery(".lostPassword .OverlayTrigger").click(function() {
-                jQuery(".xenOverlay").children("form").each(function(){
-                    if ($(this).attr("action").indexOf("contact") >= 0) {
-                        $(this).find(".fc-ctn").html("");
-                    } else if ($(this).attr("action").indexOf("lost") >= 0) {
-                        $(this).find(".fc-ctn").html("<div id='funcaptcha' data-pkey='"+fc_pkey+"'></div>");
-                    }
-                });
-                try {
-                    FunCaptcha();
-                } catch (e) {
-                    
-                }
-            });
-        }
-        </script>
-        <?php
-        $html = ob_get_clean();
-        $output = $output . $html;
-    
-
+        $output = $output . "<script src='https://funcaptcha.com/fc/api/' async defer></script>\n";
+        $output = $output . "        <script>\n";
+        $output = $output . "        if (typeof fc_query_created == 'undefined') {\n";
+        $output = $output . "            var fc_pkey = '".$options["funcaptcha_public"]."';\n";
+        $output = $output . "            var fc_query_created = true;\n";
+        $output = $output . "            jQuery(\".footerLinks .OverlayTrigger\").click(function() {\n";
+        $output = $output . "                jQuery(\".xenOverlay\").children(\"form\").each(function(){\n";
+        $output = $output . "                    if ($(this).attr(\"action\").indexOf(\"contact\") >= 0) {\n";
+        $output = $output . "                        $(this).find(\".fc-ctn\").html(\"<div id='funcaptcha' data-pkey='\"+fc_pkey+\"'></div>\");\n";
+        $output = $output . "                    } else if ($(this).attr(\"action\").indexOf(\"lost\") >= 0) {\n";
+        $output = $output . "                        $(this).find(\".fc-ctn\").html(\"\");\n";
+        $output = $output . "                    }\n";
+        $output = $output . "                });\n";
+        $output = $output . "                try {\n";
+        $output = $output . "                    FunCaptcha();\n";
+        $output = $output . "                } catch (e) {\n";
+        $output = $output . "                    \n";
+        $output = $output . "                }\n";
+        $output = $output . "            });\n";
+        $output = $output . "            jQuery(\".lostPassword .OverlayTrigger\").click(function() {\n";
+        $output = $output . "                jQuery(\".xenOverlay\").children(\"form\").each(function(){\n";
+        $output = $output . "                    if ($(this).attr(\"action\").indexOf(\"contact\") >= 0) {\n";
+        $output = $output . "                        $(this).find(\".fc-ctn\").html(\"\");\n";
+        $output = $output . "                    } else if ($(this).attr(\"action\").indexOf(\"lost\") >= 0) {\n";
+        $output = $output . "                        $(this).find(\".fc-ctn\").html(\"<div id='funcaptcha' data-pkey='\"+fc_pkey+\"'></div>\");\n";
+        $output = $output . "                    }\n";
+        $output = $output . "                });\n";
+        $output = $output . "                try {\n";
+        $output = $output . "                    FunCaptcha();\n";
+        $output = $output . "                } catch (e) {\n";
+        $output = $output . "                    \n";
+        $output = $output . "                }\n";
+        $output = $output . "            });\n";
+        $output = $output . "        }\n";
+        $output = $output . "        </script>";    
         return $output;
-		
-	}
-	public static function optionCaptchaRender(array &$extraChoices, XenForo_View $view, array $preparedOption)	{
-		$extraChoices['FunCaptcha'] = array(
-			'label'	=> 'FunCaptcha',
-			'hint'	=> 'Get Free account from <a href="http://funcaptcha.com">FunCaptcha</a>',
-			'value' => 'FunCaptcha_FunCaptcha',
-		);
-	}	
+        
+    }
+    public static function optionCaptchaRender(array &$extraChoices, XenForo_View $view, array $preparedOption) {
+        $extraChoices['FunCaptcha'] = array(
+            'label' => 'FunCaptcha',
+            'hint'  => 'Get Free account from <a href="http://funcaptcha.com">FunCaptcha</a>',
+            'value' => 'FunCaptcha_FunCaptcha',
+        );
+    }   
 }
